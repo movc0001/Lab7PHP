4921e6d01ba002a3ea5cb55b0c6879b219dacb64
movc0001 <movc0001@algonquinlive.com>
2016-12-05 12:38:49 -0500

    myPictures

--- MyPictures.php
+++ MyPictures.php
@@ -26,7 +26,7 @@
 
         $user = $_SESSION["user"];
         $userId = $user->getUserId();
-        $album = $user->getAlbums();
+        $albums = $user->getAlbums();
 
         extract($_POST);
         extract($_GET);
@@ -47,7 +47,7 @@
         } else if (isset($selectedAlbumId)) {
             $selectedAlbum = $albums[$selectedAlbumId];
 
-            if (sizeof($selectedAlbum->getPictures() == 0)) {
+            if (count($selectedAlbum->getPictures()) == 0) {
                 $noPictureMessage = "You do not have pictures in the selected album!";
             } else {
                 $selectedPictureId = array_keys($selectedAlbum->getPictures())[0];
@@ -56,7 +56,7 @@
         } else if (isset($action)) { //------------ needs to be done -----------------
             $selectedAlbumId = $selectedAlbum->getAlbum();
             $selectedPictureFilePath = ALBUM_PICTURES_DIR . "/$userId/$selectedAlbumId/" . $selectedPicture->getFileName();
-            $selectedThumbnailFilePath = ALBUM_THUMBNAILS_DIR . "/$userId/$selectedAlbumId/" . $selectePicture->getFileName();
+            $selectedThumbnailFilePath = ALBUM_THUMBNAILS_DIR . "/$userId/$selectedAlbumId/" . $selectedPicture->getFileName();
             $selectedOriginalPictureFilePath = ORIGINAL_PICTURES_DIR . "/$userId/$selectedAlbumId/" . $selectedPicture;
 
             if ($action === "Delete") {
@@ -68,7 +68,7 @@
                 unlink($selectedThumbnailFilePath);
                 unlink($selectedOriginalPictureFilePath);
 
-                if (sizeof($selectedAlbum->getPictures() == 0)) {
+                if (count($selectedAlbum->getPictures()) == 0) {
                     $noPictureMessage = "You do not have any pictures in the selected album!";
                 } else {
                     $selectePictureId = array_keys($selectedAlbum->getPictures())[0];
@@ -84,13 +84,14 @@
                 downloadFile($selectedOriginalPictureFilePath);
             }
         } else {
-            if(sizeof($albums == 0)) {
+            if (count($albums) == 0) {
                 $noAlbumMessage = "You do not have any albums yet!";
             } else {
                 $noAlbumMessage = "";
                 $selectedAlbumId = array_keys($albums)[0];
+                //$selectedAlbumId = $albums[0]->getAlbumId();
                 $selectedAlbum = $albums[$selectedAlbumId];
-                if (sizeof($selectedAlbum->getPictures() == 0)) {
+                if (count($selectedAlbum->getPictures()) == 0) {
                     $noPictureMessage = "You do not have any pictures in the selected album";
                 } else {
                     $noPictureMessage = "";
@@ -106,45 +107,59 @@
                 <div class="row vertical-margin text-center">
                     <h2>My Pictures</h2>
                 </div>
-        <?php
-        if ($noAlbumMessage != "") {
-            ?>
+                <?php
+                if ($noAlbumMessage != "") {
+                    ?>
                     <div class="row vertical-margin">
                         <div class="col-md-6 col-md-offset-3 text-center error"><?php print ($noAlbumMessage) ?></div>
                     </div>
 
-            <?php
-        } else {
-            ?>
+                    <?php
+                } else {
+                    ?>
                     <div class="row vertical-margin">
                         <div class="col-md-8">
                             <select name='selectedAlbumId' class='form-control' onchange="onAlbumChange()">
-            <?php
-            foreach ($albums as $album) {
-                $albumDisplayText = $album;
+                                <?php
+                                foreach ($albums as $album) {
+                $albumDisplayText = $album->getTitle()."Updated on ". $album->getDate_Updated();
                 $albumId = $album->getAlbumId();
-                print "<option value='$albumId' " . ($albumId == $selectedAlbumId ? "selected" : "") . ">$album"; // ------- needs to be modified ------
-            }
-            ?>
+                ?>
+                                <option value="<?php print $albumId ?>" 
+                                <?php
+
+                                    //print "<option value='$albumId' " ;//. $albumId == $selectedAlbumId ? "selected" : "" . ">$albumId"; // ------- needs to be modified ------
+                                    if ($albumId == $selectedAlbumId) {
+                                        print 'selected';
+                                    } else {
+                                        print '';
+                                    }
+                                    ?>
+                                       > 
+                                <?php   print $albumDisplayText; }?>                             
+                                    
+                                   </option>
+                                
+                                
                             </select>
                         </div>
                     </div>
 
-            <?php
-        }
-        if ($noPictureMessage != "") {
-            ?>
+                    <?php
+                }
+                if ($noPictureMessage != "") {
+                    ?>
                     <div class="row vertical-margin">
                         <div class="col-md-8 error"><?php print ($noPictureMessage) ?></div>
                     </div>
                     <?php
                 } else if ($noPictureMessage == "" && $noAlbumMessage == "") {
                     $selectedAlbumId = $selectedAlbum->getAlbumId();
-                    $selectedPictureFilePath = ALBUM_PICTURES_DIR . "/$userId/$selectedAlbumId/" . $selectePicture->getFileName();
+                    $selectedPictureFilePath = ALBUM_PICTURES_DIR . "/$userId/$selectedAlbumId/" . $selectedPicture->getFileName();
                     ?>
 
                     <div class="row vertical-margin">
-                        <div class="row text-center"><h3><?php print $selectePicture->getTitle() ?></h3></div>
+                        <div class="row col-md-8 text-center"><h3><?php print $selectedAlbum->getTitle(); ?></h3></div>
                     </div>
 
                     <div class="row vertical-margin">
@@ -161,25 +176,25 @@
                             <div class="row vertical-margin" style="white-space: nowrap">
                                 <div class="col-md-12">
                                     <div style="overflow-x: auto; overflow-y: hidden;" id="thumbnail-bar" onscroll="onThumbnailBarScroll()">
-    <?php
-    $pictures = $selectedAlbum->getPictures();
-    foreach ($pictures as $picture) {
-        $thumbnailPath = ALBUM_THUMBNAIL_DIR . "/$userId/$selectedAlbumId/" . $picture->getFileName();
-        $pictureId = $picture->getPictureId();
-        $rand = rand();
-        if ($selectedPictureId == $pictureId) {
-            print "<button value='$pictureId' name='selectedPictureId' style='border-style: solid; border-color: blue; border-width: 3px'>";
-        } else {
-            print "<button value='$pictureId' name='selectedPictureId'>";
-        }
-        print "<img style='height:65px; auto;' src='$thumbnailPath?rnd=$rand'></button>";
-    }
-    if (!isset($scrollPosition)) {
-        $scrollPosition = 0;
-    }
-    $_SESSION['selectedPicture'] = $selectePicture;
-    $_SESSION['selectedAlbum'] = $selectedAlbum;
-    ?> <!-- --------------------- --> 
+                                        <?php
+                                        $pictures = $selectedAlbum->getPictures();
+                                        foreach ($pictures as $picture) {
+                                            $thumbnailPath = ALBUM_THUMBNAIL_DIR . "/$userId/$selectedAlbumId/" . $picture->getFileName();
+                                            $pictureId = $picture->getPictureId();
+                                            $rand = rand();
+                                            if ($selectedPictureId == $pictureId) {
+                                                print "<button value='$pictureId' name='selectedPictureId' style='border-style: solid; border-color: blue; border-width: 3px'>";
+                                            } else {
+                                                print "<button value='$pictureId' name='selectedPictureId'>";
+                                            }
+                                            print "<img style='height:65px; auto;' src='$thumbnailPath?rnd=$rand'></button>";
+                                        }
+                                        if (!isset($scrollPosition)) {
+                                            $scrollPosition = 0;
+                                        }
+                                        $_SESSION['selectedPicture'] = $selectePicture;
+                                        $_SESSION['selectedAlbum'] = $selectedAlbum;
+                                        ?> <!-- --------------------- --> 
                                         <input type="hidden" name="scrollPosition" id="scrollPosition" value="<?php print $scrollPosition; ?>"/>
                                     </div>
                                 </div>
@@ -187,36 +202,36 @@
                         </div>
                         <div class="col-md-4" >
                             <div style="overflow-y: auto; overflow-x: hidden; text-align: left; max-height: 420px;">
-    <?php
-    if (trim($selectedPicture->getDescription()) != "") {
-        print "<span style='font-weight:bold' >Description:</span><p>" . $selectedPicture->getDescription();
-    }
-    if (sizeof($selectePicture->getComments()) > 0) {
-        print "<span style='font-weight:bold' >Comments:</span><br/>";
-        foreach ($selectedPicture->getComments() as $comment) {
-            $authur = $comment->getAuthur()->getname();
-            $date = $comment->getDate()->format('Y-m-d');
-            $text = $comment->getCommentText();
-            print "<p><span style='font-weight:italio; color:blue;'>$authur ($date):</span>$text</p>";
-        }
-    }
-    ?>
+                                <?php
+                                if (trim($selectedPicture->getDescription()) != "") {
+                                    print "<span style='font-weight:bold' >Description:</span><p>" . $selectedPicture->getDescription();
+                                }
+                                if (count($selectedPicture->getComments()) > 0) {
+                                    print "<span style='font-weight:bold' >Comments:</span><br/>";
+                                    foreach ($selectedPicture->getComments() as $comment) {
+                                        $authur = $comment->getAuthur()->getname();
+                                        $date = $comment->getDate()->format('Y-m-d');
+                                        $text = $comment->getCommentText();
+                                        print "<p><span style='font-weight:italio; color:blue;'>$authur ($date):</span>$text</p>";
+                                    }
+                                }
+                                ?>
                             </div>
-                            <textArea name="commentText" rows="4" style="width:100%; margin-top: 2px" plceholder="Leave a comment..."/>
+                            <textArea name="commentText" rows="4" style="width:100%; margin-top:2px" plceholder="Leave a comment..."></textArea>
                             <br/>
                             <input type="submit" value="Add Comment" class="btn btn-primary btn-min-width" name="btnComment"
                     </div>
-                                        <?php
-                                    }
-                                    ?>
+                    <?php
+                }
+                ?>
         </form>
     </div>
-                <br />
+    <br />
 </body>
 </html>
-                                    <?php
-                                    include './Lab7Common/Footer.php';
-                                    ?>
+<?php
+include './Lab7Common/Footer.php';
+?>
           
 
 
